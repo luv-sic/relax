77797bb430979b4fea1cd5fcadcb95bc6a89435e
forsigner <forsigner@gmail.com>
2018-11-12 10:26:44 +0800

    chore: improve example

--- examples/basic/src/index.js
+++ examples/basic/src/index.js
@@ -1,5 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import { createStore } from 'stamen'
+
+const CounterStore = createStore({
+  state: {
+    count: 10,
+  },
+  reducers: {
+    increment(state) {
+      state.count++
+    },
+    decrement(state) {
+      state.count--
+    },
+  },
+  effects: {
+    async asyncIncrement(dispatch) {
+      await new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve()
+        }, 1000)
+      })
+      dispatch('increment')
+    },
+  },
+})
+
+const App = () => {
+  const { get, dispatch } = CounterStore.useStore()
+  const count = get(s => s.count)
+  return (
+    <div>
+      <span>{count}</span>
+      <button onClick={() => dispatch('decrement')}>-</button>
+      <button onClick={() => dispatch('increment')}>+</button>
+      <button onClick={() => dispatch('asyncIncrement')}>async+</button>
+    </div>
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
--- examples/counter/src/components/Counter.tsx
+++ examples/counter/src/components/Counter.tsx
@@ -1,13 +1,14 @@
 import * as React from 'react'
-import { useStore } from '@stores/CounterStore'
+import CounterStore from '@stores/CounterStore'
 
 const Counter = () => {
-  const { get, dispatch } = useStore()
+  const { get, dispatch } = CounterStore.useStore()
   const count = get(s => s.count)
-
+  console.log('render...')
   return (
     <div className="box">
       <div style={{ textAlign: 'center' }}>
+        <div>{count}</div>
         <div>{count}</div>
         <button onClick={() => dispatch(a => a.decrement)}>-</button>
         <button onClick={() => dispatch('increment')}>+</button>
